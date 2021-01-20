cac516c57989793f4073b830edeb828e472f016f
kherney <kevinh939@gmail.com>
2021-01-20 07:40:30 -0500

    [ADD] compute, related functions to Char and many2one fields. Make python logic constrain to isbn field.

--- models/bookstore_book.py
+++ models/bookstore_book.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.exceptions import Warning
+from odoo.exceptions import Warning, ValidationError
 
 
 class Bookstore(models.Model):
@@ -35,6 +35,36 @@ class Bookstore(models.Model):
     book_publisher = fields.Many2one('res.partner', string="Publisher")
     book_author_ids = fields.Many2many('res.partner',
                                        string="Authors")
+    book_publisher_country_related = fields.Many2one(comodel_name="res.country",
+                                                     related="book_publisher.country_id",
+                                                     readonly=True,
+                                                     store=False,)
+    book_publisher_city = fields.Char(string="Ciudad",
+                                      related="book_publisher.city")
+    book_publisher_country_id = fields.Many2one(comodel_name="res.country",
+                                                string="Publisher Country",
+                                                compute="_compute_publisher_country",
+                                                inverse="_inverse_publisher_country",
+                                                search="_search_publisher_country",
+                                                store=False,)
+
+    @api.depends("book_publisher.country_id")
+    def _compute_publisher_country(self):
+        for book in self:
+            book.book_publisher_country_id = book.book_publisher.country_id
+
+    def _inverse_publisher_country(self):
+        for book in self:
+            book.book_publisher.country_id = book.book_publisher_country_id
+
+    def _search_publisher_country(self, operator, value):
+        return [('book_publisher', operator, value)]
+
+    @api.constrains('book_isbn')
+    def _constrain_isbn_valid(self):
+        for book in self:
+            if book.book_isbn and not book._check_isbn():
+                raise ValidationError("{} no is a valide ISBN value".format(book.book_isbn))
 
     def _check_isbn(self):
         self.ensure_one()
@@ -51,5 +81,5 @@ class Bookstore(models.Model):
             if not record.book_isbn:
                 raise Warning("Please provide a ISBN to {}".format(record.book_name))
             elif record.book_isbn and not self._check_isbn():
-                raise Warning("Please provide a vañide ISBN tp {}".format(record.book_name))
+                raise Warning("Please provide a valide ISBN tp {}".format(record.book_name))
         return True
